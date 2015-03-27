316d0651a7c8e38b0e45172900e22089b65effc3
Tim Hunold <timhunold@primitivelogic.com>
2015-03-27 14:50:27 -0700

    [major] VCARD
    turned site to VCARD

--- src/js/src/controllers/MasterController.js
+++
@@ -1,9 +0,0 @@
-
-angular.module('app.controllers.master', []).controller('MasterController', ['$cookies','$scope','$log', function ($cookies, $scope, $log) {
-  
-  'use strict';
-
-  $log.debug('MasterController()');
-
-}]);
-
--- src/js/src/directives/directives.js
+++
@@ -1,6 +0,0 @@
-
-angular.module('app.directives', [
-  'app.directives.static_popover',
-  'app.directives.right_side_nav',
-  'app.directives.json_text'
-]);
