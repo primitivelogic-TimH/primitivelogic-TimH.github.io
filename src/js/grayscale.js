0bc9ed0ced1423032dd97dd2f2d9841f94adeae3
Tim Hunold <hunold@google.com>
2019-05-10 15:22:56 -0700

    clean up src formatting

--- src/js/grayscale.js
+++ src/js/grayscale.js
@@ -1,65 +1,88 @@
 // On window load. This waits until images have loaded which is essential
-$(window).load(function(){
-	
-	// Fade in images so there isn't a color "pop" document load and then on window load
-	$(".menu_img img").animate({opacity:1},500);
-	
-	// clone image
-	$('.menu_img img, .author_foto img, .hover_img img').each(function(){
-		var el = $(this);
-		el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"1","opacity":"0"}).insertBefore(el).queue(function(){
-			var el = $(this);
-			el.parent().css({"width":this.width,"height":this.height});
-			el.dequeue();
-		});
-		this.src = grayscale(this.src);
-	});
-	
-	// Fade menu image 
-	$('.toggle').mouseover(function(){
-		$(this).find('.img_grayscale').stop().animate({opacity:1}, 500);
-	})
-	$('.toggle').mouseout(function(){
-		$(this).find('img:first').stop().animate({opacity:0}, 500);
-	});
+$(window).load(function() {
+  // Fade in images so there isn't a color "pop" document load and then on window load
+  $(".menu_img img").animate({ opacity: 1 }, 500);
 
-	// Fade author foto 
-	$('.author_foto').mouseover(function(){
-		$(this).find('.img_grayscale').stop().animate({opacity:1}, 500);
-	})
-	$('.author_foto').mouseout(function(){
-		$(this).find('img:first').stop().animate({opacity:0}, 500);
-	});
-	
-	// Fade portfolio images 
-	$('.projects .element').mouseover(function(){
-		$(this).find('.img_grayscale').stop().animate({opacity:1}, 300);
-	})
-	$('.projects .element').mouseout(function(){
-		$(this).find('img:first').stop().animate({opacity:0}, 300);
-	});
-	
+  // clone image
+  $(".menu_img img, .author_foto img, .hover_img img").each(function() {
+    var el = $(this);
+    el.css({ position: "absolute" })
+      .wrap("<div class='img_wrapper' style='display: inline-block'>")
+      .clone()
+      .addClass("img_grayscale")
+      .css({ position: "absolute", "z-index": "1", opacity: "0" })
+      .insertBefore(el)
+      .queue(function() {
+        var el = $(this);
+        el.parent().css({ width: this.width, height: this.height });
+        el.dequeue();
+      });
+    this.src = grayscale(this.src);
+  });
+
+  // Fade menu image
+  $(".toggle").mouseover(function() {
+    $(this)
+      .find(".img_grayscale")
+      .stop()
+      .animate({ opacity: 1 }, 500);
+  });
+  $(".toggle").mouseout(function() {
+    $(this)
+      .find("img:first")
+      .stop()
+      .animate({ opacity: 0 }, 500);
+  });
+
+  // Fade author foto
+  $(".author_foto").mouseover(function() {
+    $(this)
+      .find(".img_grayscale")
+      .stop()
+      .animate({ opacity: 1 }, 500);
+  });
+  $(".author_foto").mouseout(function() {
+    $(this)
+      .find("img:first")
+      .stop()
+      .animate({ opacity: 0 }, 500);
+  });
+
+  // Fade portfolio images
+  $(".projects .element").mouseover(function() {
+    $(this)
+      .find(".img_grayscale")
+      .stop()
+      .animate({ opacity: 1 }, 300);
+  });
+  $(".projects .element").mouseout(function() {
+    $(this)
+      .find("img:first")
+      .stop()
+      .animate({ opacity: 0 }, 300);
+  });
 });
 
 // Grayscale w canvas method
-function grayscale(src){
-	var canvas = document.createElement('canvas');
-	var ctx = canvas.getContext('2d');
-	var imgObj = new Image();
-	imgObj.src = src;
-	canvas.width = imgObj.width;
-	canvas.height = imgObj.height; 
-	ctx.drawImage(imgObj, 0, 0); 
-	var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	for(var y = 0; y < imgPixels.height; y++){
-		for(var x = 0; x < imgPixels.width; x++){
-			var i = (y * 4) * imgPixels.width + x * 4;
-			var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-			imgPixels.data[i] = avg; 
-			imgPixels.data[i + 1] = avg; 
-			imgPixels.data[i + 2] = avg;
-		}
-	}
-	ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-	return canvas.toDataURL();
+function grayscale(src) {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+  var imgObj = new Image();
+  imgObj.src = src;
+  canvas.width = imgObj.width;
+  canvas.height = imgObj.height;
+  ctx.drawImage(imgObj, 0, 0);
+  var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  for (var y = 0; y < imgPixels.height; y++) {
+    for (var x = 0; x < imgPixels.width; x++) {
+      var i = y * 4 * imgPixels.width + x * 4;
+      var avg =
+        (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+      imgPixels.data[i] = avg;
+      imgPixels.data[i + 1] = avg;
+      imgPixels.data[i + 2] = avg;
+    }
+  }
+  ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+  return canvas.toDataURL();
 }
--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,64 +1,58 @@
-$(document).ready(function(){
-	//Menu
-	$.fn.slideFadeToggle = function(options, callback) {
-	if (typeof callback == 'function') { // make sure the callback is a function
-		   callback.call(this); // brings the scope to the callback
-	   }
-	   return this.animate({opacity: 'toggle', height: 'toggle'}, 400);
-	};
-	jQuery(document).ready(function() {
-		jQuery(".toggle").next(".hidden").hide();
-		
-		jQuery(".toggle").click(function() {
-			$('.current').not(this).toggleClass('current').next('.hidden').slideFadeToggle();
-			$(this).toggleClass('current').next().slideFadeToggle(500, function(){
-			var this_element = $(this);
-			setTimeout(function(){
-			var scroll_to = this_element.prev('.toggle').offset().top;
-			$('html, body').animate({scrollTop: scroll_to }, 500);
-			}, 500);
-			});
-		});
-	});
-	
-	
-	//Contact form
-	$("#ajax-contact-form").submit(function() {
-		var str = $(this).serialize();		
-		$.ajax({
-			type: "POST",
-			url: "contact_form/contact_process.php",
-			data: str,
-			success: function(msg) {
-				// Message Sent - Show the 'Thank You' message and hide the form
-				if(msg == 'OK') {
-					result = '<div class="notification_ok">Your message has been sent. Thank you!</div>';
-					$("#fields").hide();
-				} else {
-					result = msg;
-				}
-				$('#note').html(result);
-			}
-		});
-		return false;
-	});
-	
-	
-	//Background Video
-	jQuery(".player").mb_YTPlayer();
+$(document).ready(function() {
+  //Menu
+  $.fn.slideFadeToggle = function(options, callback) {
+    if (typeof callback == "function") {
+      // make sure the callback is a function
+      callback.call(this); // brings the scope to the callback
+    }
+    return this.animate({ opacity: "toggle", height: "toggle" }, 400);
+  };
+  jQuery(document).ready(function() {
+    jQuery(".toggle")
+      .next(".hidden")
+      .hide();
 
+    jQuery(".toggle").click(function() {
+      $(".current")
+        .not(this)
+        .toggleClass("current")
+        .next(".hidden")
+        .slideFadeToggle();
+      $(this)
+        .toggleClass("current")
+        .next()
+        .slideFadeToggle(500, function() {
+          var this_element = $(this);
+          setTimeout(function() {
+            var scroll_to = this_element.prev(".toggle").offset().top;
+            $("html, body").animate({ scrollTop: scroll_to }, 500);
+          }, 500);
+        });
+    });
+  });
+
+  //Contact form
+  $("#ajax-contact-form").submit(function() {
+    var str = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "contact_form/contact_process.php",
+      data: str,
+      success: function(msg) {
+        // Message Sent - Show the 'Thank You' message and hide the form
+        if (msg == "OK") {
+          result =
+            '<div class="notification_ok">Your message has been sent. Thank you!</div>';
+          $("#fields").hide();
+        } else {
+          result = msg;
+        }
+        $("#note").html(result);
+      }
+    });
+    return false;
+  });
+
+  //Background Video
+  jQuery(".player").mb_YTPlayer();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
